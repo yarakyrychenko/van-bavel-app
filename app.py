705b41d500a630248c33d36c963e75a3f4f0b6f6
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-27 22:25:36 -0400

    change to shill

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from google.oauth2 import service_account
-from gsheetsdb import connect
+from shillelagh.backends.apsw.db import connect
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -11,17 +11,20 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+cursor = conn.cursor()
+
+st.title(" Life vs Twitter Identity ") 
+st.subheader("See how your 'real' identity compares to your Twitter identity.")
+st.text_input("Enter a twitter username to begin", key="name")
 
 sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(f"{row.i_am}")
+
+SQL = """
+    SELECT *
+    FROM "{sheet_url}"
+    """
+for row in cursor.execute(SQL):
+    print(row)
+
+
