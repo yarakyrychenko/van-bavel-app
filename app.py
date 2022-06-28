ff908f5f11d156b85fbf1c8dbfc89ec9da958466
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 19:23:58 -0400

    mysql

--- app.py
+++ app.py
@@ -1,42 +1,26 @@
+# streamlit_app.py
+
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
+import mysql.connector
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
 
-st.title(" Life vs Twitter Identity ") 
-st.subheader("See how your 'real' identity compares to your Twitter identity.")
-st.text_input("Enter a twitter username to begin", key="name")
+conn = init_connection()
 
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
 def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-cursor = conn.cursor()
+rows = run_query("SELECT * from mytable;")
 
-def add_row(query):
-    conn.execute(query, headers=1)
-    
-
-sheet_url = st.secrets["public_gsheets_url"]
-add_row(f"""
-    INSERT INTO "{sheet_url}"
-    VALUES ('1', {st.session_state.name}, 'rep', 'star')""")
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-
+# Print results.
 for row in rows:
-    st.write(f"{row.i_am} has a :{row.i_am}:")
-
+    st.write(f"{row[0]} has a :{row[1]}:")
