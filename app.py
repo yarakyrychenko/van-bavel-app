c90605c2ae1126cbab1a1294a7eb25d65526bc40
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-27 22:30:44 -0400

    uh

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from google.oauth2 import service_account
-from shillelagh.backends.apsw.db import connect
+from gsheetsdb import connect
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -11,20 +11,24 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials)
 
-cursor = conn.cursor()
-
 st.title(" Life vs Twitter Identity ") 
 st.subheader("See how your 'real' identity compares to your Twitter identity.")
 st.text_input("Enter a twitter username to begin", key="name")
 
 sheet_url = st.secrets["private_gsheets_url"]
 
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
-SQL = """
-    SELECT *
-    FROM "{sheet_url}"
-    """
-for row in cursor.execute(SQL):
-    print(row)
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
+# Print results.
+for row in rows:
+    st.write(f"{row.i_am} has a :{row.i_am}:")
 
