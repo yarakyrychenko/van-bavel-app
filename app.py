5d6262a084108e524ffda5c6163e93b7d8fabdcf
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 23:17:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ st.title(" Are you partisan? ")
 st.subheader("See how you and other twitter users see each party visualized in wordclouds ")
 st.text_input("Enter a twitter username to begin", key="name")
 st.session_state.party = st.radio(
-     "Which party do you identify with?",
-     ('Independant','Republican', 'Democratic'))
+     "How do you identify?",
+     ('Independant','Republican', 'Democrat'))
 
 dem_words = []
-st.text("Please add ten words that describe Democrats best in your opinion:")
+st.text("Please add five words that describe Democrats best in your opinion:")
 for i in range(5):
     dem_words.append(st.text_input("D"+str(i+1)))
 st.session_state.dem_words = ", ".join(dem_words)
 st.text(f"your words are {st.session_state.dem_words}")
 
 rep_words = []
-st.text("Please add ten words that describe Republicans best in your opinion:")
+st.text("Please add five words that describe Republicans best in your opinion:")
 for i in range(5):
-    rep_words.append(st.text_input("R"+str(i+1)))
+    rep_words.append(st.text_input("R"+str(i+1),key = "R"+str(i+1)))
 st.session_state.rep_words = ", ".join(rep_words)
 st.text(f"your words are {st.session_state.rep_words}")
 
