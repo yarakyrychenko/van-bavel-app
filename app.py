cf329d09b00acd3d9a05bffa8d86511cd176e5ec
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 22:20:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
+from google.oauth2 import service_account
 from shillelagh.backends.apsw.db import connect
 
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+
 # Create a connection object.
-conn = connect(":memory:")
+conn = connect(credentials=credentials)
 
 sheet_url = st.secrets["public_gsheets_url"]
 query = f'SELECT * FROM "{sheet_url}"'
