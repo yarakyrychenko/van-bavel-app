60ab730e2c833e92cbc59b3296b2440eeac76f0f
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 22:14:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ conn = connect(":memory:")
 
 sheet_url = st.secrets["public_gsheets_url"]
 query = f'SELECT * FROM "{sheet_url}"'
-
+insert = f"""INSERT INTO "{sheet_url}" 
+            VALUES (1, 'no', 'dem', 'rabbit')
+"""
 # Print results.
+conn.execute(insert)
+
 for row in conn.execute(query):
     st.write(f"{row}")
