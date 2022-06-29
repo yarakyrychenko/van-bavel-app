c458a93e49254972e07b6c1d163df258b99f3088
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 22:11:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ from shillelagh.backends.apsw.db import connect
 # Create a connection object.
 conn = connect(":memory:")
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
 sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+query = f'SELECT * FROM "{sheet_url}"'
 
 # Print results.
-for row in rows:
-    st.write(f"{row.party} has a :{row.i_am}:")
+for row in conn.execute(query):
+    st.write(f"{row}")
