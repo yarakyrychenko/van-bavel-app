d1ac270f6f4bf3322522c7405aa1b789bae14926
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 23:51:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from datetime import datetime
 from uuid import uuid4
 
 st.title(" Are you partisan? ") 
-st.subheader("See how you and other twitter users see each party visualized in wordclouds ")
+st.header("See how you and other twitter users see each party visualized in wordclouds ")
 st.text_input("Enter a twitter username to begin", key="name")
 st.session_state.party = st.radio(
      "How do you identify?",
      ('Independant','Republican', 'Democrat'))
 
 dem_words = []
-st.text("Please add five words that describe Democrats best in your opinion:")
+st.subheader("Please add five words that describe Democrats best in your opinion:")
 for i in range(5):
     dem_words.append(st.text_input("D"+str(i+1)))
 st.session_state.dem_words = ", ".join(dem_words)
-st.text(f"your words are {st.session_state.dem_words}")
+st.subheader(f"Your words are {st.session_state.dem_words}")
 
 rep_words = []
-st.text("Please add five words that describe Republicans best in your opinion:")
+st.subheader("Please add five words that describe Republicans best in your opinion:")
 for i in range(5):
     rep_words.append(st.text_input("R"+str(i+1),key = "R"+str(i+1)))
 st.session_state.rep_words = ", ".join(rep_words)
-st.text(f"your words are {st.session_state.rep_words}")
+st.subheader(f"Your words are {st.session_state.rep_words}")
 
 if st.button("Submit", key='submit'):
     st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
@@ -49,8 +49,7 @@ if st.button("Submit", key='submit'):
     for row in conn.execute(query):
         all_dem_words.append(row[3])
         all_rep_words.append(row[4])
-    st.text(str(all_dem_words))
-    st.text(str(all_rep_words))
+
     all_dem_words = ", ".join(all_dem_words)
     all_dem_words = all_dem_words.split(", ")
     all_rep_words = ", ".join(all_rep_words)
