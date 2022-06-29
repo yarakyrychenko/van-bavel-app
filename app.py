95154665ce769bb03ddd8565861ab06b50694b59
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-28 22:09:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from gsheetsdb import connect
+from shillelagh.backends.apsw.db import connect
 
 # Create a connection object.
-conn = connect()
+conn = connect(":memory:")
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
