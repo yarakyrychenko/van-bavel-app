ea19f2a1bf46b5a41afe4e02b07a78b97637f61b
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-06-30 19:07:21 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -15,8 +15,8 @@ def make_dataframe(executed_query):
     cols = ["party", "dem_words", "rep_words", "dem_temp", "rep_temp"]
     df = pd.DataFrame(columns=cols)
     for row in executed_query:
-        st.write(type(row))
-        df1 = pd.DataFrame(list(row)[2:], columns=cols)
+        df1 = pd.DataFrame(row[range(2,7)], columns=cols)
+        st.write(df1)
         df = pd.concat([df, df1], ignore_index=True)
 
     return df
