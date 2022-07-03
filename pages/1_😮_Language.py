b56c3292370f58b42599162a9c941136ae3795de
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-07-03 01:27:52 -0500

    add moral emotional

--- pages/1_😮_Language.py
+++ pages/1_😮_Language.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from helper import *
 from shillelagh.backends.apsw.db import connect
+from twanalysis import *
 
 st.set_page_config(
     page_title="Language",
@@ -19,4 +20,37 @@ st.title("😮 Language 😮")
 if 'conn' not in st.session_state:
     st.warning("Please go to the homepage and add a twitter username")
 elif 'df' not in st.session_state:
-    pass
+    with st.spinner(text="In progress..."):
+        sheet_url = st.secrets["private_gsheets_url"]
+        query = f'SELECT * FROM "{sheet_url}"'
+        st.session_state.df = make_dataframe(st.session_state.conn.execute(query))
+
+if "all_stopwords" not in st.session_state:
+     st.session_state.all_stopwords = make_worddict("dictionaries/all_stopwords.txt")
+     st.session_state.moral_emotional = make_worddict("dictionaries/moral_emotional.txt")
+
+if "api" not in st.session_state:    
+            st.session_state.api = authenticate(st.secrets["consumer_key"],
+                                            st.secrets["consumer_secret"],
+                                            st.secrets["access_token_key"],
+                                            st.secrets["access_token_secret"])
+
+if 'df' in st.session_state:
+    try:
+        outtweets = get_user_tweeets(st.session_state.name,st.session_state.api)
+         
+        try:
+            cat = outtweets[9]  
+            
+            with st.spinner(text='We\'re analyzing the tweets. Give it a sec...'):
+                figure, all_text = make_wordcloud(st.session_state.all_stopwords, outtweets)
+                n_moral_emotional = count_words(all_text, st.session_state.moral_emotional)
+            st.pyplot(figure)
+            st.markdown(f"{total/len(all_text.split())}\% of words you used are moral emotional.")
+
+                
+        except:
+            st.markdown("This account has fewer than 10 tweets. Tweet more and come back later or try again.")  
+            
+    except:
+            st.markdown("This account doesn't exist. Please try again.")        
---
+++ twanalysis.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt 
+import tweepy, re
+from wordcloud import WordCloud
+from collections import Counter
+
+def make_worddict(file_name):
+    text_file = open(file_name, "r")
+    stopwords_list = text_file.read().split("\n")
+    text_file.close()
+    return set(stopwords_list)
+     
+def authenticate(consumer_key, consumer_secret,access_token_key, access_token_secret):    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token_key, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+def get_user_tweeets(screen_name,api):
+    alltweets = [] 
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    alltweets.extend(new_tweets)
+    outtweets = [tweet.text for tweet in alltweets] 
+    return outtweets
+
+def preprocess(out):
+    text = " ".join(out)
+    text = re.sub(pattern=r"http\S+",repl="",string=text.lower())
+    text = re.sub(pattern=r"@\S+",repl="",string=text)
+    return text
+
+
+def make_wordcloud(st_words, out):
+    text = preprocess(out)
+    wordcloud = WordCloud(width=1800, height=1200,stopwords=st_words,
+                        max_font_size=250, max_words=100, background_color="white",
+                        colormap='cool', collocations=True).generate(text)  
+
+    fig = plt.figure(figsize=(18,12))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    return fig, text
+
+def count_words(all_text,dict):
+    counter = Counter(all_text) 
+    keys = counter.keys()
+    total = 0
+    for word in dict:
+        word = word.split("*")[0]
+        numbers = [counter[key] for key in keys in key.startswith(word)]
+        total += sum(numbers)
+    return total
